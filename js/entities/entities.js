86685a92669d4a1b8d458658417bea356a84082c
riad29 <rd974@georgetown.edu>
2016-06-17 11:07:38 -0400

    Update entities.js

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -9,7 +9,7 @@ var BirdEntity = me.Entity.extend({
 
         this._super(me.Entity, 'init', [x, y, settings]);
         this.alwaysUpdate = true;
-        this.body.gravity = 2.0;
+        this.body.gravity = 10.0;
         this.gravityForce = 0.01;
         this.maxAngleRotation = Number.prototype.degToRad(30);
         this.maxAngleRotationDown = Number.prototype.degToRad(90);
